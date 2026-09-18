f5454147e4507bb9e9ed898880e2106a94ce7754
ribeirovaurnino@icloud.com <ribeirovaurnino@icloud.com>
2026-09-19 00:18:56 +0200

    correction fosse_detector.gd

--- Fosse/fosse_detector.gd
+++ Fosse/fosse_detector.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_fosse_body_entered(body: Node2D) -> void:
+	if body.is_in_group("balle")	:
+		get_tree().change_scene_to_file("res://ecran/ecran_defaite.tscn")
