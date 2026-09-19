bed844e54d68bb566479e0198d43966cf27ab488
ribeirovaurnino@icloud.com <ribeirovaurnino@icloud.com>
2026-09-19 17:41:26 +0200

    randomisation de l'angle de l'impulsion de départ (oui j'ai été aidé...)

--- balle/balle_rebondissement.gd
+++ balle/balle_rebondissement.gd
@@ -4,7 +4,9 @@ extends Node
 
 func _ready() -> void:
 	var parent = get_parent()
-	parent.linear_velocity = Vector2(1, 1) * vitesse
+	
+	var angle := randf_range(PI / 4.0, 3.0 * PI / 4.0)  # oui c'est pas de moi, j'avais aucune foutu idée de comment faire (et pas le temps de me prendre la tête y'a des délais de rendu quoi
+	parent.linear_velocity = Vector2.from_angle(angle) * vitesse
 	
 	
 	
