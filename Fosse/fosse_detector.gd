842238805d01add9e5b8b771d95aa47459c697ff
ribeirovaurnino@icloud.com <ribeirovaurnino@icloud.com>
2026-09-19 14:39:39 +0200

    et c'est la victoire

--- Fosse/fosse_detector.gd
+++ Fosse/fosse_detector.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
--- briques/detecteur balle/collision_balle.gd
+++ briques/detecteur balle/collision_balle.gd
@@ -7,10 +7,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("balle"):
 		get_parent().get_parent().queue_free() 
+		if (get_tree().get_nodes_in_group("brique").size()==0) :      #le nb restant de briques
+			get_tree().change_scene_to_file("res://ecran_victoire.tscn")
