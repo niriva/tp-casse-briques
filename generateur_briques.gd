9799493f2163a61e12dffd58566ded569bb96fd0
ribeirovaurnino@icloud.com <ribeirovaurnino@icloud.com>
2026-09-19 16:13:42 +0200

    ajout générateur_brique export var

--- generateur_briques.gd
+++ generateur_briques.gd
@@ -1,10 +1,17 @@
 extends Node
 
 @export var scene_brique : PackedScene
+@export var colonnes :=6
+@export var lignes := 4
+@export var marge_gauche := 60.0
+@export var marge_droite := 60.0
+@export var marge_haut := 25.0
+@export var marge_bas := 250.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var ecran := get_viewport().get_visible_rect().size
 	pass # Replace with function body.
 
 
